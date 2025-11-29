b2c62acab5a15b39d20164bef49a5296ad050663
Giacomo Baldessari <103346031+gbaldessari@users.noreply.github.com>
2025-11-29 10:33:47 -0300

    Add new route 'nuevo' to AppController
    Introduces a new GET endpoint '/nuevo' in AppController that returns a static string response. This expands the API with an additional route.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  getNuevo(): string {
+    return 'Esta es una nueva ruta!';
+  }
 }
